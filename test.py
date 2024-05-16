2de7d16dd5774fdf98cb64b330921d2ee9e73896
QuangNguyen2910 <nguyenquang71103@gmail.com>
2024-05-17 00:39:43 +0700

    Add test.py function to check GPU memory and usage

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
